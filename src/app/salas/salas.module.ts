5b4688bf1cd7c9d9194a4c31c87917d1552dddf8
Sebasjaga <sebastian.jaramillo03@uceva.edu.co>
2023-11-26 22:44:19 -0500

    Se creó el componente listar-sala

--- src/app/salas/salas.module.ts
+++ src/app/salas/salas.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrearSalaComponent } from './crear-sala/crear-sala.component';
 import { DetalleSalaComponent } from './detalle-sala/detalle-sala.component';
+import { ListarSalaComponent } from './listar-sala/listar-sala.component';
 
 
 
 @NgModule({
   declarations: [
     CrearSalaComponent,
-    DetalleSalaComponent
+    DetalleSalaComponent,
+    ListarSalaComponent
   ],
   imports: [
     CommonModule
